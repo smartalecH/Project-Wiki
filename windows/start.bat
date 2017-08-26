8d4193c1d1008f25231c28bd8be31c72d108d362
GNHua <gonghuabupt@gmail.com>
2017-08-25 21:33:54 -0600

    add start caddy.exe command

--- windows/start.bat
+++ windows/start.bat
@@ -6,9 +6,11 @@ net start MongoDB
 :: navigate to Project-Wiki directory
 cd %~dp0\..
 
-:: start server
+:: start server in the background
 start pythonw run.py
-
 echo Server started
+echo To shut down server, close this console, and run stop.bat as administator.
 
+:: start caddy with configuration in Caddyfile
+caddy.exe -conf Caddyfile
 sleep 1
